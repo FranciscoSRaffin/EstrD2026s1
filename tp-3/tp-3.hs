79ea3fa9cc2d8d0b9c5b6e9796ca40dea43311d7
Francisco Raffin <francisco.s.raffin@gmail.com>
2026-04-14 21:25:50 -0300

    Tp-3 2-10

---
+++ tp-3/tp-3.hs
@@ -0,0 +1,158 @@
+unoSiCeroSino :: Bool -> Int
+unoSiCeroSino True = 1
+unoSiCeroSino _    = 0
+
+singularSi :: a -> Bool -> [a]
+singularSi x True  = x:[]
+singularSi x False = []
+
+-- TIPOS RECURSIVOS SIMPLES
+
+-- 1 Celdas con bolitas
+
+data Color = Azul | Rojo
+data Celda = Bolita Color Celda | CeldaVacia
+
+-- 1.1
+nroBolitas :: Color -> Celda -> Int
+nroBolitas c CeldaVacia  = 0
+nroBolitas c (Bolita co ce) = (unoSiCeroSino (sonMismoColor c co)) + nroBolitas co ce
+
+sonMismoColor :: Color -> Color -> Bool
+sonMismoColor Azul Azul = True
+sonMismoColor Rojo Rojo = True
+sonMismoColor _    _    = False
+
+-- 1.2
+poner :: Color -> Celda -> Celda
+poner co c = (Bolita co c)
+
+-- 1.3
+sacar :: Color -> Celda -> Celda
+sacar _ CeldaVacia    = CeldaVacia
+sacar c1 (Bolita c ce) = if (sonMismoColor c1 c)
+                          then ce
+                          else (Bolita c (sacar c1 ce))
+
+-- 1.4
+ponerN :: Int -> Color -> Celda -> Celda
+ponerN n co ce = if (n > 0)
+    then (Bolita co (ponerN (n-1) co ce))
+    else ce
+
+-- 2 Camino hacia el tesoro
+
+data Objeto = Cacharro | Tesoro
+data Camino = Fin | Cofre [Objeto] Camino | Nada Camino
+
+esTesoro :: Objeto -> Bool
+esTesoro Tesoro = True
+esTesoro _      = False
+
+-- 2.1
+hayTesoro :: Camino -> Bool
+hayTesoro (Cofre co ca) = (hayTesoroCofre co) || (hayTesoro ca)
+hayTesoro (Nada ca) =  hayTesoro ca
+hayTesoro _ = False
+
+hayTesoroCofre :: [Objeto] -> Bool
+hayTesoroCofre []   = False
+hayTesoroCofre (o:os) = esTesoro o || hayTesoroCofre os
+
+-- 2.2
+pasosHastaTesoro :: Camino -> Int
+pasosHastaTesoro (Nada ca) = 1 + pasosHastaTesoro ca
+pasosHastaTesoro (Cofre co ca) = if (hayTesoroCofre co)
+    then 0
+    else 1 + pasosHastaTesoro ca
+pasosHastaTesoro _ = 0
+
+-- 2.3
+hayTesoroEn :: Int -> Camino -> Bool
+hayTesoroEn p (Cofre co ca) = (p == 0 && hayTesoroCofre co) || hayTesoroEn (p-1) ca
+hayTesoroEn p (Nada ca)     = p > 0 && hayTesoroEn (p-1) ca
+hayTesoroEn _ _             = False
+
+-- 2.4
+alMenosNTesoros :: Int -> Camino -> Bool
+alMenosNTesoros cant (Nada ca)     = cant == 0 || alMenosNTesoros cant ca  
+alMenosNTesoros cant (Cofre co ca) = (cant - cantidadDeTesoros co) <= 0 || (alMenosNTesoros (cant - cantidadDeTesoros co) ca) 
+alMenosNTesoros cant _             = cant == 0
+
+cantidadDeTesoros :: [Objeto] -> Int
+cantidadDeTesoros []   = 0
+cantidadDeTesoros (o:os) = unoSiCeroSino(esTesoro o) + cantidadDeTesoros os
+
+-- TIPOS DE ARBOLES
+
+-- 2 Arboles binarios
+
+data Tree a = EmptyT | NodeT a (Tree a) (Tree a)
+
+esEmptyT :: Tree a -> Bool 
+esEmptyT EmptyT = True
+esEmptyT _      = False
+
+contenido :: Tree a -> a
+contenido EmptyT          = error "El arbol no puede ser vacio" 
+contenido (NodeT n t1 t2) = n 
+
+-- 2.1
+sumarT :: Tree Int -> Int
+sumarT EmptyT          = 0
+sumarT (NodeT n t1 t2) = n + sumarT t1 + sumarT t2
+
+-- 2.2
+sizeT :: Tree a -> Int
+sizeT EmptyT          = 0
+sizeT (NodeT _ t1 t2) = 1 + sizeT t1 + sizeT t2 
+
+-- 2.3
+mapDobleT :: Tree Int -> Tree Int
+mapDobleT EmptyT          = EmptyT 
+mapDobleT (NodeT n t1 t2) = (NodeT (n*2) (mapDobleT t1) (mapDobleT t2))
+
+-- 2.4
+perteneceT :: Eq a => a -> Tree a -> Bool
+perteneceT _ EmptyT          = False
+perteneceT a (NodeT b t1 t2) = (a == b) || (perteneceT a t1) || (perteneceT a t2) 
+
+-- 2.5
+aparicionesT :: Eq a => a-> Tree a-> Int
+aparicionesT _ EmptyT          = 0
+aparicionesT a (NodeT b t1 t2) = unoSiCeroSino (a == b) + aparicionesT a t1 + aparicionesT a t2
+
+-- 2.6
+leaves :: Tree a -> [a]
+leaves EmptyT          = []
+leaves (NodeT b t1 t2) = (singularSi b (esEmptyT t1 && esEmptyT t2)) ++ leaves t1 ++ leaves t2
+
+-- 2.7 
+heightT :: Tree a -> Int
+heightT EmptyT          = 0
+heightT (NodeT _ t1 t2) = max (length (leaves t1)) (length (leaves t2))
+
+-- 2.8 
+mirrorT :: Tree a -> Tree a
+mirrorT EmptyT          = EmptyT
+mirrorT (NodeT a t1 t2) = (NodeT a (mirrorT t2) (mirrorT t1))
+
+-- 2.9 
+toList :: Tree a -> [a]
+toList EmptyT          = EmptyT 
+toList (NodeT a t1 t2) = (singularSi (not esEmptyT t1) (contenido t1)) ++ [a] ++ (singularSi (!esEmptyT t2) (contenido t2)) 
+
+-- 2.10
+levelN :: Int-> Tree a -> [a]
+levelN _ EmptyT          = []
+levelN l (NodeT a t1 t2) = if (l == 0) then l : (levelN l t1) : (levelN l t2) : []
+                                           else (levelN l t1) : (levelN l t2)
+
+-- 2.11 
+listPerLevel :: Tree a -> [[a]]
+listPerLevel t = agruarNiveles (heightT t) t 
+
+-- WIP
+agruparNiveles :: Int -> Tree a -> [[a]]
+agruparNiveles _ EmptyT         = [[]]
+agruparNiveles l (NodeT a t1 t2) = [[]]
