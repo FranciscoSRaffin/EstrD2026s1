fb357d85b662ee225fe87db3b3e66bc2da77f445
Francisco Raffin <francisco.s.raffin@gmail.com>
2026-05-29 19:15:02 -0300

    TP 9 ej 4 in progress

---
+++ tp-9/tp-9/4/4.cpp
@@ -0,0 +1,18 @@
+
+// Iterativa
+void printN(int n, string s)
+
+void cuentaRegresiva(int n)
+
+void desdeCeroHastaN(int n)
+
+int mult(int n, int m)
+
+void primerosN(int n, string s)
+
+bool pertenece(char c, string s)
+
+int apariciones(char c, string s)
+
+
+// Recursiva
