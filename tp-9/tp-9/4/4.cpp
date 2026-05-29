63dde3089e99848c7fbd26935cb463b614e129be
Francisco Raffin <francisco.s.raffin@gmail.com>
2026-05-29 19:17:44 -0300

    TP 9 ej 4 in progress

--- tp-9/tp-9/4/4.cpp
+++ tp-9/tp-9/4/4.cpp
@@ -1,14 +1,38 @@
 
 // Iterativa
-void printN(int n, string s)
+void printN(int n, string s) {
+    while (n > 0) {
+        cout s << endl;;
+        n--
+    }
+}
 
-void cuentaRegresiva(int n)
+void cuentaRegresiva(int n) {
+    while (n > 0) {
+        cout n << endl;;
+        n--
+    }
+}
 
-void desdeCeroHastaN(int n)
+void desdeCeroHastaN(int n) {
+    int x = 0;
 
-int mult(int n, int m)
+    while (n !== x) {
+        cout x << endl;;
+        x++
+    }
+}
 
-void primerosN(int n, string s)
+int mult(int n, int m) {
+    while (n > 0) {
+        m = m + m
+    }
+}
+
+void primerosN(int n, string s) {
+    
+}
+
 
 bool pertenece(char c, string s)
 
