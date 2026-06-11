070816170a5cc017407202b1093a8fd31852f779
Francisco Raffin <francisco.s.raffin@gmail.com>
2026-06-10 22:15:07 -0300

    TP 11 Ej 1

---
+++ tp-11/tp-11/1/LinkedList.cpp
@@ -0,0 +1,95 @@
+struct NodoL;
+struct LinkedListSt;
+
+typedef LinkedListSt* LinkedList;
+struct IteratorSt;
+
+typedef IteratorSt* ListIterator;
+
+LinkedList nil(){
+    LinkedList list = new LinkedList;
+    list->cantidad = 0;
+    list->primero = NULL;
+    return list;
+};
+
+bool isEmpty(LinkedList xs){
+    return xs->cantidad == 0;
+};
+
+int head(LinkedList xs){
+    return xs->primero;
+};
+
+void Cons(int x, LinkedList xs){
+    NodoL nuevoSegundo = xs->primero // xs->primero --> NodoL | NULL
+    xs->primero = new NodoL;
+    xs->primero->elem = x;
+    xs->primero->siguiente = nuevoSegundo
+};
+
+void Tail(LinkedList xs){
+    NodoL primerElemento = xs->primero->elem
+    xs->primero->elem = primerElemento->siguiente
+    delete primerElemento;
+    xs->cantidad--;
+};
+
+int length(LinkedList xs){
+    return xs->cantidad;
+};
+
+void Snoc(int x, LinkedList xs){
+    NodeL* nuevoNodo = new nuevoNodoL;
+    nuevoNodo->elem = x;
+    nuevoNodo->siguiente = NULL; 
+    if (xs->primero == NULL) { xs->primero = nuevoNodo } 
+    else {
+        NodoL nodoActual = xs->primero;
+        while (nodoActual->siguiente != NULL) {
+            nodoActual = nodoActual->siguiente
+        };
+        nodoActual->siguiente = nuevoNodo;
+    };
+    xs->cantiddad++;
+};
+
+ListIterator getIterator(LinkedList xs){
+    ListIterator ixs = new ListIterator;
+    ixs->current = xs->primero;
+    return ixs;
+};
+
+int current(ListIterator ixs){
+    return ixs->current->elem;
+};
+
+void SetCurrent(int x, ListIterator ixs){
+    ixs->current->elem = x;
+};
+
+void Next(ListIterator ixs){
+    ixs->current = ixs->current->siguiente;
+};
+
+bool atEnd(ListIterator ixs){
+    return ixs->current->siguiente == NULL;
+};
+
+void DisposeIterator(ListIterator ixs){
+    delete xls;
+};
+
+void DestroyL(LinkedList xs){
+    if (xs->primero == NULL) {
+        delete xs
+    } else {
+        NodoL nodoActual = xs->primero;
+        while (nodoActual->siguiente != NULL) {
+            nodoADescartar = nodoActual
+            nodoActual = nodoActual->siguiente
+            delete nodoADescartar;
+        };
+    }
+    delete xs
+};
